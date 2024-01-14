eb515f7434bf337c35be554afd9dd7ad8c00839a
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-01-14 13:02:11 +0530

    hello world

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 2;
+    float c = 45.4;
+    double d = 45.34238525792308;
+
+    int x = a*b + c / d;
+    printf("Hello, World!!!\n");
+    printf("%d", x);
+
+
+    printf("\n");
+    return 0;
+}
