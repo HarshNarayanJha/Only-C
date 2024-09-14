478ce008eeabd38244598b0f58f99064c6eead1f
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-14 23:26:22 +0530

    add linear queue

---
+++ DSA/queue.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+#define MAX 5
+
+typedef struct {
+  int list[MAX];
+  int rear;
+  int front;
+} queue;
+
+int isEmpty(queue* q) {
+  return q->front == -1 && q->rear == -1;
+}
+
+int isFull(queue* q) {
+  return q->rear == MAX - 1;
+}
+
+void enqueue(queue* q, int val) {
+  if (isFull(q)) {
+    printf("Queue Overflow\n");
+    return;
+  }
+
+  if (isEmpty(q)) {
+    q->rear = q->front = 0;
+  } else {
+    q->rear++;
+  }
+
+  q->list[q->rear] = val;
+}
+
+int dequeue(queue* q) {
+  int first = 0;
+  if (isEmpty(q)) {
+    printf("Queue Underflow\n");
+    return -1;
+  }
+
+  if (q->front == q->rear) {
+    first = q->list[q->front];
+    q->rear = q->front = -1;
+  } else {
+    first = q->list[q->front];
+    q->front++;
+  }
+
+  return first;
+}
+
+void print(queue* q) {
+
+  if (isEmpty(q)) {
+    return;
+  }
+
+  for (int i = q->front; i < q->rear + 1; i++) {
+    printf("%d -> ", q->list[i]);
+  }
+
+  printf("\n");
+}
+
+int main() {
+  queue q;
+  q.rear = -1;
+  q.front = -1;
+
+  enqueue(&q, 1);
+  enqueue(&q, 2);
+  enqueue(&q, 3);
+  printf("%d ", dequeue(&q));
+  enqueue(&q, 4);
+  enqueue(&q, 5);
+  enqueue(&q, 6);
+
+  print(&q);
+
+  printf("%d ", dequeue(&q));
+  printf("%d ", dequeue(&q));
+  printf("%d ", dequeue(&q));
+  printf("%d ", dequeue(&q));
+  printf("%d ", dequeue(&q));
+
+  print(&q);
+
+  return 0;
+}
---
+++ DSA/reverselinkedlist.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* reverseList(struct Node* head) {
+	struct Node *curr = head, *prev = NULL, *next;
+
+	while (curr != NULL) {
+		next = curr->next;
+		curr->next = prev;
+
+		prev = curr;
+		curr = next;
+	}
+
+	return prev;
+}
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf(" %d", node->data);
+        node = node->next;
+    }
+}
+
+
+struct Node* createNode(int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+int main() {
+
+    // Create a hard-coded linked list:
+    // 1 -> 2 -> 3 -> 4 -> 5
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+
+    printf("Given Linked list:");
+    printList(head);
+
+    head = reverseList(head);
+
+    printf("\nReversed Linked List:");
+    printList(head);
+
+    return 0;
+}
