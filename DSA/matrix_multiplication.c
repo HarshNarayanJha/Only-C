a6d7744b56d01d4831e972eab677321b388899c8
Harsh Narayan Jha <harshnj@proton.me>
2025-02-15 17:31:05 +0530

    Divide and Conquer O(n^3) multiplication algorithm

--- DSA/matrix_multiplication.c
+++ DSA/matrix_multiplication.c
@@ -4,8 +4,11 @@
 
 void input_matrix(int mat[N][N], int m, int n);
 void print_matrix(int mat[N][N], int m, int n);
+void add_matrix(int A[N][N], int B[N][N], int C[N][N], int m, int n);
 void multiply_matrix(
     int A[N][N], int B[N][N], int C[N][N], int am, int an, int bm, int bn);
+
+void multiply_matrix_dnc(int A[N][N], int B[N][N], int C[N][N], int n);
 
 int main() {
   int am = 0, an = 0, bm = 0, bn = 0;
@@ -40,6 +43,12 @@ int main() {
 
   printf("\nResult of multiplication:\n");
   print_matrix(R, am, bn);
+
+  printf("\nMultiplying using DnC R = AxB ...\n\n");
+  multiply_matrix_dnc(A, B, R, am);
+
+  printf("\nResult of multiplication:\n");
+  print_matrix(R, am, bn);
 }
 
 void multiply_matrix(
@@ -54,6 +63,40 @@ void multiply_matrix(
       }
     }
   }
+}
+
+void multiply_matrix_dnc(int A[N][N], int B[N][N], int C[N][N], int n) {
+  if (n <= 2) {
+    C[0][0] = A[0][0] * B[0][0] + A[0][1] * B[1][0];
+    C[0][1] = A[0][0] * B[0][1] + A[0][1] * B[1][1];
+    C[1][0] = A[1][0] * B[0][0] + A[1][1] * B[1][0];
+    C[1][1] = A[1][0] * B[0][1] + A[1][1] * B[1][1];
+    return;
+  }
+
+  int A11[N][N], A12[N][N], A21[N][N], A22[N][N];
+  int B11[N][N], B12[N][N], B21[N][N], B22[N][N];
+  int C11[N][N], C12[N][N], C21[N][N], C22[N][N];
+  int C110[N][N], C111[N][N], C120[N][N], C121[N][N], C210[N][N], C211[N][N],
+      C220[N][N], C221[N][N];
+
+  multiply_matrix_dnc(A11, B11, C110, n / 2);
+  multiply_matrix_dnc(A12, B21, C111, n / 2);
+  add_matrix(C110, C111, C11, n / 2, n / 2);
+
+  multiply_matrix_dnc(A11, B12, C120, n / 2);
+  multiply_matrix_dnc(A12, B22, C121, n / 2);
+  add_matrix(C120, C121, C12, n / 2, n / 2);
+
+  multiply_matrix_dnc(A21, B11, C210, n / 2);
+  multiply_matrix_dnc(A22, B21, C211, n / 2);
+  add_matrix(C210, C211, C21, n / 2, n / 2);
+
+  multiply_matrix_dnc(A21, B12, C220, n / 2);
+  multiply_matrix_dnc(A22, B22, C221, n / 2);
+  add_matrix(C220, C221, C22, n / 2, n / 2);
+
+  return;
 }
 
 void input_matrix(int mat[N][N], int m, int n) {
@@ -71,5 +114,13 @@ void print_matrix(int mat[N][N], int m, int n) {
       printf("%d ", mat[i][j]);
     }
     printf("\n");
+  }
+}
+
+void add_matrix(int A[N][N], int B[N][N], int C[N][N], int m, int n) {
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      C[i][j] = A[i][j] + B[i][j];
+    }
   }
 }
